c1349ac002f6a12d99041e2b5cc64c613f85ba11
SickanK <sixtenklementsson@protonmail.com>
2021-12-29 01:49:18 +0100

    added websocket with backoff

---
+++ capybara/src/utils/improvedWebSocket.ts
@@ -0,0 +1,208 @@
+type Protocols = string | string[] | undefined;
+type WebSocketData = string | ArrayBuffer | Blob | ArrayBufferView;
+
+enum ReadyState {
+  Connecting = 0,
+  Open = 1,
+  Closing = 2,
+  Closed = 3,
+}
+
+enum WebSocketEvents {
+  Close = "close",
+  Error = "error",
+  Message = "message",
+  Open = "open",
+  Retry = "retry",
+}
+
+interface RetryEvent {
+  retries: number;
+  maxRetries: number;
+  initialDelay: number;
+  delay: number;
+}
+
+interface WebSocketEventMap {
+  close: CloseEvent;
+  error: Event;
+  message: MessageEvent<any>;
+  open: Event;
+  retry: RetryEvent;
+}
+
+type EventListener<K extends WebSocketEvents> = (
+  instance: ImprovedWebSocket,
+  ev: WebSocketEventMap[K]
+) => any;
+
+interface WebSocketEventListeners {
+  open: EventListener<WebSocketEvents.Open>[];
+  close: EventListener<WebSocketEvents.Close>[];
+  error: EventListener<WebSocketEvents.Error>[];
+  message: EventListener<WebSocketEvents.Message>[];
+  retry: EventListener<WebSocketEvents.Retry>[];
+}
+
+interface Backoff {
+  initialDelay?: number;
+  maxRetries?: number;
+}
+
+class ImprovedWebSocket {
+  private url: string;
+  private protocols: Protocols;
+  private backoff: Backoff | undefined;
+  private retries: number | undefined;
+  private delay: number | undefined;
+  private maxRetries: number | undefined;
+  private initialDelay: number | undefined;
+  private eventListeners: WebSocketEventListeners = {
+    open: [],
+    close: [],
+    error: [],
+    message: [],
+    retry: [],
+  };
+  private ws?: WebSocket;
+
+  constructor(url: string, protocols?: Protocols, backoff?: Backoff) {
+    this.url = url;
+    this.protocols = protocols;
+    this.backoff = backoff;
+
+    if (backoff !== undefined) {
+      this.retries = 0;
+      this.delay =
+        backoff.initialDelay === undefined ? 100 : backoff.initialDelay;
+      this.initialDelay = this.delay;
+      this.maxRetries =
+        backoff.maxRetries === undefined ? 8 : backoff.maxRetries;
+    }
+
+    this.tryConnect();
+  }
+
+  public bufferedAmount(): number | undefined {
+    if (this.ws) return this.ws.bufferedAmount;
+    return undefined;
+  }
+
+  public readyState(): ReadyState | undefined {
+    if (this.ws) return this.ws.readyState;
+    return undefined;
+  }
+
+  public send(data: WebSocketData): void {
+    if (this.ws) this.ws.send(data);
+  }
+
+  public close(code?: number, reason?: string): void {
+    if (this.ws) this.ws.close(code, reason);
+  }
+
+  public addEventListener<K extends WebSocketEvents>(
+    type: K,
+    listener: EventListener<K>
+  ): this {
+    (this.eventListeners[type] as EventListener<K>[]).push(listener);
+
+    return this;
+  }
+
+  public removeEventListener<K extends WebSocketEvents>(
+    type: K,
+    listener: EventListener<K>
+  ): this {
+    (this.eventListeners[type] as EventListener<K>[]) = (
+      this.eventListeners[type] as EventListener<K>[]
+    ).filter((l: EventListener<K>) => {
+      return l !== listener;
+    });
+
+    return this;
+  }
+
+  private tryConnect(): void {
+    if (this.ws) {
+      this.ws.removeEventListener("open", this.onOpen);
+      this.ws.removeEventListener("close", this.onClose);
+      this.ws.removeEventListener("message", this.onMessage);
+      this.ws.removeEventListener("error", this.onError);
+    }
+
+    this.ws = new WebSocket(this.url, this.protocols);
+    this.ws.addEventListener("open", this.onOpen);
+    this.ws.addEventListener("close", this.onClose);
+    this.ws.addEventListener("message", this.onMessage);
+    this.ws.addEventListener("error", this.onError);
+  }
+
+  private onOpen = (ev: WebSocketEventMap["open"]) =>
+    this.handleEvent(WebSocketEvents.Open, ev);
+
+  private onClose = (ev: WebSocketEventMap["close"]) =>
+    this.handleEvent(WebSocketEvents.Close, ev);
+
+  private onMessage = (ev: WebSocketEventMap["message"]) =>
+    this.handleEvent(WebSocketEvents.Message, ev);
+
+  private onError = (ev: WebSocketEventMap["error"]) =>
+    this.handleEvent(WebSocketEvents.Error, ev);
+
+  private onRetry = (ev: WebSocketEventMap["retry"]) =>
+    this.handleEvent(WebSocketEvents.Retry, ev);
+
+  private handleEvent<K extends WebSocketEvents>(
+    type: K,
+    ev: WebSocketEventMap[K]
+  ) {
+    if (
+      (type === WebSocketEvents.Close && (ev as CloseEvent).code === 1006) ||
+      !(ev as CloseEvent).wasClean ||
+      (ev as CloseEvent).reason === ""
+    ) {
+      this.reconnect();
+    }
+
+    if (type === WebSocketEvents.Open) {
+      this.retries = 0;
+      this.delay = this.initialDelay;
+    }
+
+    this.dispatchEvent<K>(type, ev);
+  }
+
+  private dispatchEvent<K extends WebSocketEvents>(
+    type: K,
+    ev: WebSocketEventMap[K]
+  ) {
+    (this.eventListeners[type] as EventListener<K>[]).forEach(
+      (listener: EventListener<K>) => {
+        listener(this, ev);
+      }
+    );
+  }
+
+  private reconnect() {
+    if (this.backoff === undefined || this.retries === this.maxRetries) return;
+
+    setTimeout(() => {
+      if (this.retries && this.maxRetries && this.delay && this.initialDelay) {
+        this.dispatchEvent(WebSocketEvents.Retry, {
+          retries: this.retries,
+          maxRetries: this.maxRetries,
+          delay: this.delay,
+          initialDelay: this.initialDelay,
+        });
+
+        this.retries += 1;
+        this.delay *= 2;
+
+        this.tryConnect();
+      }
+    }, this.delay);
+  }
+}
+
+export default ImprovedWebSocket;
